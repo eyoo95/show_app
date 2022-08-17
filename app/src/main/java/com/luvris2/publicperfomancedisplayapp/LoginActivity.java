137ba25733de80782acbe0f9c24d557d1475960f
eyoo95 <105832377+eyoo95@users.noreply.github.com>
2022-08-17 11:42:26 +0900

    add

--- app/src/main/java/com/luvris2/publicperfomancedisplayapp/LoginActivity.java
+++ app/src/main/java/com/luvris2/publicperfomancedisplayapp/LoginActivity.java
@@ -6,7 +6,6 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.telecom.Call;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
