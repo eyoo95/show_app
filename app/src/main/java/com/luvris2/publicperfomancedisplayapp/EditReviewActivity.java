20d5d7541e0af02b6162faa37a48a07f0485b7e3
CongSeung <leem1121@gmail.com>
2022-08-17 17:41:21 +0900

    add

---
+++ app/src/main/java/com/luvris2/publicperfomancedisplayapp/EditReviewActivity.java
@@ -0,0 +1,14 @@
+package com.luvris2.publicperfomancedisplayapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_review);
+    }
+}
