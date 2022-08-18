00d75cc591e24770ee85f28c3cd735017ab75fd8
CongSeung <leem1121@gmail.com>
2022-08-18 17:44:15 +0900

    merge

---
+++ app/src/main/java/com/luvris2/publicperfomancedisplayapp/LikeActivity.java
@@ -0,0 +1,14 @@
+package com.luvris2.publicperfomancedisplayapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LikeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_like);
+    }
+}
