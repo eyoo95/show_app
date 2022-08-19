6e5b64bbed1e4f2c72101d01cd1a3b2bdf54455e
CongSeung <leem1121@gmail.com>
2022-08-19 17:17:48 +0900

    Working on UI object ID ... 01

---
+++ app/src/main/java/com/luvris2/publicperfomancedisplayapp/BoardReadActivity.java
@@ -0,0 +1,14 @@
+package com.luvris2.publicperfomancedisplayapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BoardReadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board_read_activiry);
+    }
+}
