502bb8fe075c3b1c23677f570229663b6ad2e1ae
luvris2 <luvris2@naver.com>
2022-08-30 15:11:04 +0900

    add4_All_merge_project_20220830_1507

--- app/src/main/java/com/luvris2/publicperfomancedisplayapp/api/KopisPerformanceApi.java
+++ app/src/main/java/com/luvris2/publicperfomancedisplayapp/api/KopisPerformanceApi.java
@@ -4,18 +4,33 @@ import com.luvris2.publicperfomancedisplayapp.model.KopisApiPerformance;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface KopisPerformanceApi {
     // 공연 검색
     @GET("/performancesearch")
-    Call<KopisApiPerformance> getPlaceSearch(@Query("stdate") String stdate,          // 시작일
-                                             @Query("eddate") String eddate,          // 종료일
-                                             @Query("cpage") int cpage,               // 전체페이지
-                                             @Query("rows") int rows,                 // 페이지 당 목록 수
-                                             @Query("shprfnm") String shprfnm,        // 공연 명
-                                             @Query("shprfnmfct") String shprfnmfct,  // 공연 시설 명
-                                             @Query("shcate") String shcate,          // 장르 코드
-                                             @Query("signgucode") String signgucode,  // 시도 코드
-                                             @Query("prfstate") int prfstate);        // 공연 상태
+    Call<KopisApiPerformance> getPerformance(@Query("stdate") String stdate,
+                                             @Query("eddate") String eddate,
+                                             @Query("cpage") int cpage,
+                                             @Query("rows") int rows,
+                                             @Query("shprfnm") String shprfnm,
+                                             @Query("shprfnmfct") String shprfnmfct,
+                                             @Query("shcate") String shcate,
+                                             @Query("signgucode") String signgucode,
+                                             @Query("signgusubcod") String signgusubcode,
+                                             @Query("prfstate") int prfstate);
+
+    // 공연 상세 검색
+    @GET("/performancedetail/{prfId}")
+    Call<KopisApiPerformance> getPerformanceDetail(@Path("prfId") String prfId);
+
+    // 내 지역(구) 공연 검색
+    @GET("/nearbyperformance/{sidoCodeSub}")
+    Call<KopisApiPerformance> nearByPlaceSearch(@Path("sidoCodeSub") String sidoCodeSub,
+                                                @Query("stdate") String stdate,
+                                                @Query("eddate") String eddate,
+                                                @Query("cpage") int cpage,
+                                                @Query("rows") int rows,
+                                                @Query("prfstate") int prfstate);
 }
--- app/src/main/java/com/luvris2/publicperfomancedisplayapp/model/KopisApiPerformance.java
+++ app/src/main/java/com/luvris2/publicperfomancedisplayapp/model/KopisApiPerformance.java
@@ -1,13 +1,35 @@
 package com.luvris2.publicperfomancedisplayapp.model;
 
+import androidx.annotation.Keep;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 
 public class KopisApiPerformance implements Serializable {
-    private String mt20id, prfnm, prfStartDate, prfEndDate, fcltynm, poster, genrenm, prfstate;
+    //    mt20id	    공연ID
+    //    prfnm	        공연명
+    //    prfpdfrom	    공연시작일
+    //    prfpdto	    공연종료일
+    //    fcltynm	    공연시설명(공연장명)
+    //    prfcast	    공연출연진
+    //    prfcrew	    공연제작진
+    //    prfruntime    공연 런타임
+    //    prfage	    공연 관람 연령
+    //    entrpsnm	    제작사
+    //    pcseguidance  티켓가격
+    //    poster	    포스터이미지경로
+    //    genrenm	장르
+    //    prfstate	공연상태
+    //    dtguidance	공연시간
+    // latitude, longitude 내 위도 경도
+    // resultList API 통신시 배열로 오는 응답 결과를 저장하는 변수
+    // result API 통신시 오브젝트로 오는 응답 결과를 저장하는 변수
+    private String mt20id, prfnm, fcltynm, poster, genrenm, prfstate;
+    private String prfcast, prfcrew, prfruntime, prfage, entrpsnm, pcseguidance, dtguidance;
     private String prfpdfrom, prfpdto;
+    private String latitude, longitude;
     private ArrayList<KopisApiPerformance> resultList;
-    private KopisApiPerformance resultListObj;
+    private KopisApiPerformance result;
 
 
     public KopisApiPerformance() {
@@ -15,23 +37,25 @@ public class KopisApiPerformance implements Serializable {
     public KopisApiPerformance(ArrayList<KopisApiPerformance> resultList) {
         this.resultList = resultList;
     }
-    public KopisApiPerformance(KopisApiPerformance resultList) {
-        this.resultListObj = resultList;
+    public KopisApiPerformance(KopisApiPerformance result) {
+        this.result = result;
     }
 
 
     public ArrayList<KopisApiPerformance> getResultList() {
         return resultList;
     }
+
     public void setResultList(ArrayList<KopisApiPerformance> resultList) {
         this.resultList = resultList;
     }
 
-    public KopisApiPerformance getResultListObj() {
-        return resultListObj;
+    public KopisApiPerformance getResult() {
+        return result;
     }
-    public void setResultList(KopisApiPerformance resultList) {
-        this.resultListObj = resultList;
+
+    public void setResult(KopisApiPerformance result) {
+        this.result = result;
     }
 
     public String getPrfpdfrom() {
@@ -62,20 +86,6 @@ public class KopisApiPerformance implements Serializable {
         this.prfnm = prfnm;
     }
 
-    public String getPrfStartDate() {
-        return prfStartDate;
-    }
-    public void setPrfStartDate(String prfStartDate) {
-        this.prfStartDate = prfStartDate;
-    }
-
-    public String getPrfEndDate() {
-        return prfEndDate;
-    }
-    public void setPrfEndDate(String prfEndDate) {
-        this.prfEndDate = prfEndDate;
-    }
-
     public String getPrfPlace() {
         return fcltynm;
     }
@@ -102,5 +112,75 @@ public class KopisApiPerformance implements Serializable {
     }
     public void setPrfState(String prfstate) {
         this.prfstate = prfstate;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getPrfcast() {
+        return prfcast;
+    }
+
+    public void setPrfcast(String prfcast) {
+        this.prfcast = prfcast;
+    }
+
+    public String getPrfcrew() {
+        return prfcrew;
+    }
+
+    public void setPrfcrew(String prfcrew) {
+        this.prfcrew = prfcrew;
+    }
+
+    public String getPrfruntime() {
+        return prfruntime;
+    }
+
+    public void setPrfruntime(String prfruntime) {
+        this.prfruntime = prfruntime;
+    }
+
+    public String getPrfage() {
+        return prfage;
+    }
+
+    public void setPrfage(String prfage) {
+        this.prfage = prfage;
+    }
+
+    public String getEntrpsnm() {
+        return entrpsnm;
+    }
+
+    public void setEntrpsnm(String entrpsnm) {
+        this.entrpsnm = entrpsnm;
+    }
+
+    public String getPcseguidance() {
+        return pcseguidance;
+    }
+
+    public void setPcseguidance(String pcseguidance) {
+        this.pcseguidance = pcseguidance;
+    }
+
+    public String getDtguidance() {
+        return dtguidance;
+    }
+
+    public void setDtguidance(String dtguidance) {
+        this.dtguidance = dtguidance;
     }
 }
