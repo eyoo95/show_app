17651a8030d70e25ad28ce39c70b99ad39f8e2d8
Choi Ji Hun <100457604+Apocachip@users.noreply.github.com>
2022-09-02 13:11:02 +0900

    add

--- app/src/main/java/com/luvris2/publicperfomancedisplayapp/adapter/PartyAdapter.java
+++ app/src/main/java/com/luvris2/publicperfomancedisplayapp/adapter/PartyAdapter.java
@@ -4,23 +4,32 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.luvris2.publicperfomancedisplayapp.R;
 import com.luvris2.publicperfomancedisplayapp.model.PartyData;
+import com.luvris2.publicperfomancedisplayapp.model.Posting;
+import com.luvris2.publicperfomancedisplayapp.ui.PartyActivity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PartyAdapter extends RecyclerView.Adapter<PartyAdapter.ViewHolder>{
 
     Context context;
-    List<PartyData> partydataList;
+    List<PartyData> partyDataList;
 
-    public PartyAdapter(Context context, List<PartyData> partydataList) {
+    String myNickName;
+
+    public PartyAdapter(Context context, List<PartyData> partydataList, String myNickName) {
         this.context = context;
-        this.partydataList = partydataList;
+        this.partyDataList = partydataList;
+
+        //닉네임을 받아넣는 파라미터
+        this.myNickName = myNickName;
     }
 
     @NonNull
@@ -33,18 +42,49 @@ public class PartyAdapter extends RecyclerView.Adapter<PartyAdapter.ViewHolder>{
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        PartyData partyData = partyDataList.get(position);
 
+        holder.txtNickName.setText(partyData.getNickname());
+        holder.txtMsg.setText(partyData.getMsg());
+
+        //내가 메세지를 보냈다면 오른쪽 끝에서 보이고
+        //남이 보냈다면 왼쪽 끝에서 보임
+        if(partyData.getNickname().equals(this.myNickName)) {
+            holder.txtMsg.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
+            holder.txtNickName.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
+        }
+        else {
+            holder.txtMsg.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
+            holder.txtNickName.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+
+        //삼항 연산자
+        return partyDataList == null ? 0 :  partyDataList.size();
+    }
+
+    public PartyData getChat(int position) {
+        return partyDataList != null ? partyDataList.get(position) : null;
+    }
+
+    public void addChat(PartyData chat) {
+        partyDataList.add(chat);
+        notifyItemInserted(partyDataList.size()-1); //갱신
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
+        TextView txtNickName;
+        TextView txtMsg;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            txtNickName = itemView.findViewById(R.id.txtNickName);
+            txtMsg = itemView.findViewById(R.id.txtMsg);
         }
     }
 }
--- app/src/main/java/com/luvris2/publicperfomancedisplayapp/ui/PartyActivity.java
+++ app/src/main/java/com/luvris2/publicperfomancedisplayapp/ui/PartyActivity.java
@@ -1,17 +1,103 @@
 package com.luvris2.publicperfomancedisplayapp.ui;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.luvris2.publicperfomancedisplayapp.R;
+import com.luvris2.publicperfomancedisplayapp.adapter.PartyAdapter;
+import com.luvris2.publicperfomancedisplayapp.model.PartyData;
+
+import java.util.ArrayList;
 
 public class PartyActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    EditText editChat;
+    Button btnSend;
+
+    private String nick = "nick2";
+
+    // 어댑터, 리스트
+    PartyAdapter adapter;
+    ArrayList<PartyData> partyDataList = new ArrayList<>();
+
+    DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference commandsRef = rootRef.child("rooms").child("chat");
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_party);
+
+        editChat = findViewById(R.id.editChat);
+        btnSend = findViewById(R.id.btnSend);
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(PartyActivity.this));
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String msg = editChat.getText().toString(); //msg
+
+                if(msg != null) {
+                    PartyData chat = new PartyData();
+                    chat.setNickname(nick);
+                    chat.setMsg(msg);
+                    commandsRef.push().setValue(chat);
+                }
+            }
+        });
+
+        adapter = new PartyAdapter(PartyActivity.this, partyDataList, nick);
+
+        adapter.notifyDataSetChanged();
+
+        recyclerView.setAdapter(adapter);
+
+        commandsRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Log.d("CHATCHAT", dataSnapshot.getValue().toString());
+                PartyData chat = dataSnapshot.getValue(PartyData.class);
+                adapter.addChat(chat);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
